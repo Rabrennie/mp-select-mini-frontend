9cb6fd0cedf89f941d0344effd9574399aa3e8ca
Rabrennie <rennierab@gmail.com>
2017-09-12 21:50:56 +0100

    add vue resource to vue object

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueWebsocket from 'vue-native-websocket';
+import VueResource from 'vue-resource';
 import App from './App';
 import router from './router';
 import store from './store';
@@ -10,6 +11,8 @@ Vue.config.productionTip = false;
 
 Vue.use(VueWebsocket, 'ws://192.168.1.194:81', { store });
 store.$socket = Vue.prototype.$socket;
+
+Vue.use(VueResource);
 
 /* eslint-disable no-new */
 new Vue({
